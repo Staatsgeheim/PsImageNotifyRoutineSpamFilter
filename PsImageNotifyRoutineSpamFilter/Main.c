9b4b7f664d76af6f7d8c2c0dc496451a48315cd8
Staatsgeheim <maarten.boone@gmail.com>
2016-01-15 01:04:20 +0100

    Updated copyright year

--- PsImageNotifyRoutineSpamFilter/Main.c
+++ PsImageNotifyRoutineSpamFilter/Main.c
@@ -1,7 +1,7 @@
 /*
 	PsImageNotifyRoutine Spam Filter v1.0
 
-	Copyright (c) 2015 Maarten Boone
+	Copyright (c) 2016 Maarten Boone
 	Permission is hereby granted, free of charge, to any person
 	obtaining a copy of this software and associated documentation
 	files (the "Software"), to deal in the Software without
